c60374c87514efeb854614fac0fc6d16021ad2a7
iSame7 <mabrouksameh@gmail.com>
2016-05-09 23:53:21 +0200

    Added unit test case.

--- CIExampleTests/CIExampleTests.swift
+++ CIExampleTests/CIExampleTests.swift
@@ -26,11 +26,6 @@ class CIExampleTests: XCTestCase {
         XCTAssert(a + 1 == 2)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
